83de1502422a1c28813d6881f466cca34b1d8179
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2026-04-15 20:49:29 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
+# =========================
+# Cell 3: App Factory
+# =========================
+
 from flask import Flask, jsonify
 from app.config import Config
 from app.extensions import db, migrate, cors
-from app.routes import recommendations_bp
+from app.routes.recommendations import recommendations_bp
 
 
 def create_app():
